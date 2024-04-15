3bb411e815b50cfcf4e46d3a11a786bd55cd1976
Nalthyr <114312397+DanBlash@users.noreply.github.com>
2024-04-14 20:46:11 -0400

    added a few more endpoints for website

--- pages/home.js
+++ pages/home.js
@@ -5,3 +5,15 @@ const knowledgeButton = document.getElementById("knowledge-card");
 knowledgeButton.addEventListener("click", () => {
     window.location.href = "knowledge.html";
 })
+
+const aboutButton = document.getElementById("about-card");
+
+aboutButton.addEventListener("click", () => {
+    window.location.href = "about.html";
+})
+
+const socialButton = document.getElementById("social-card");
+
+socialButton.addEventListener("click", () => {
+    window.location.href = "social.html";
+})
