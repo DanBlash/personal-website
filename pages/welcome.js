ddda5c912c74020deefde1cf7c64b9b2a830321f
Nalthyr <114312397+DanBlash@users.noreply.github.com>
2024-04-16 21:54:08 -0400

    exit animation for welcome page, changed coloring, need to figure out reload on back button press for welcome page

--- pages/welcome.js
+++ pages/welcome.js
@@ -1,6 +1,14 @@
-
 const welcomeButton = document.getElementById("welcome-Button");
+const buttonContainer = document.querySelector(".container");
 
 welcomeButton.addEventListener("click", () => {
-    window.location.href = "home.html";
+    if (!buttonContainer.classList.contains("slide-out-fwd-center")) {
+        buttonContainer.classList.toggle("slide-out-fwd-center");
+    }
+    setTimeout(() => {
+        window.location.href = "home.html";
+    }, 1000);
+
+    
 })
+
